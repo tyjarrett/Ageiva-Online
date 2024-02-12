ad979490f224a4d44a9bb25dbe585d085fdd3719
LoganMartinez <lmartin9@trinity.edu>
2024-02-12 17:48:03 -0600

    added queries to healthmodel get request

--- AgingHealthBackend/healthModel/views.py
+++ AgingHealthBackend/healthModel/views.py
@@ -52,10 +52,26 @@ class HealthDataView(APIView):
     return Response(status=status.HTTP_201_CREATED)
   
   def get(self, request):
+    query = request.GET.get("vars", "")
+    vars = query.split(",") if query else []
     background = get_object_or_404(models.BackgroundData, user=request.user)
     health_data = models.HealthData.objects.filter(user=request.user).order_by("date")
-    ser_background = serializers.BackgroundDataSerializer(background)
-    ser_health_data = serializers.HealthDataSerializer(health_data, many=True)
-    response = {"background": ser_background.data, "health_data": ser_health_data.data}
+    
+    
+    if len(vars) == 0:
+      ser_background = serializers.BackgroundDataSerializer(background)
+      ser_health_data = serializers.HealthDataSerializer(health_data, many=True)
+      response = {"background": ser_background.data, "health_data": ser_health_data.data}
+    else:
+      fields_to_include = ["id", "date", "age"]
+      res_background = {var: getattr(background, var) for var in fields_to_include + vars if var in fields_to_include + constants.background_variables}
+      res_health_data = []
+      for d in health_data:
+        res_health_data.append({var: getattr(d, var) for var in fields_to_include + vars if var in fields_to_include + constants.health_variables})
+
+      response = { "background": res_background, "health_data": res_health_data }
+
+
+
     return Response(response)
     
