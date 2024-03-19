74b4994314a203c2ec484b51463ac40aa80467e8
LoganMartinez <lmartin9@trinity.edu>
2024-03-18 21:32:38 -0500

    changed some styling

--- AgingHealthApp/components/results/HealthDataChart.tsx
+++ AgingHealthApp/components/results/HealthDataChart.tsx
@@ -4,8 +4,10 @@ import {
   VictoryLine,
   VictoryTheme,
   VictoryAxis,
+  VictoryLegend,
 } from "victory-native";
-import { StyleSheet } from "react-native";
+import { VictoryThemeDefinition } from "victory-core";
+import { surveyQuestions } from "../../utilities/constants";
 
 type Props = {
   label: string;
@@ -13,15 +15,73 @@ type Props = {
   numPoints: number;
 };
 
+const varColor = "#005AB5";
+const meanColor = "#DC3220";
+const graphColor = "#FFFFFF";
+
+const graphTheme: VictoryThemeDefinition = {
+  ...VictoryTheme.grayscale,
+  axis: {
+    style: {
+      axis: { stroke: graphColor, strokeWidth: 0.5 },
+      ticks: { stroke: graphColor },
+      tickLabels: { fill: graphColor },
+      axisLabel: { fill: graphColor },
+    },
+  },
+  legend: {
+    ...VictoryTheme.grayscale.legend,
+    style: {
+      ...VictoryTheme.grayscale.legend?.style,
+      labels: {
+        fill: graphColor,
+        fontFamily: "sans-serif",
+        fontSize: 12,
+        letterSpacing: "normal",
+        padding: 10,
+        stroke: "transparent",
+      },
+    },
+  },
+};
+console.log(VictoryTheme.grayscale.legend?.style?.labels);
+
 const HealthDataChart = ({ label, data, numPoints }: Props) => {
   const dataPoints = data.slice(0, numPoints).map((datum) => ({
     x: datum.date,
     y: datum.value,
   }));
+  const variableQuery = surveyQuestions.filter((v) => v.variableId === label);
+  const variableMean = variableQuery.length > 0 ? variableQuery[0].mean : null;
+  const meanLine = variableMean
+    ? [
+        { x: dataPoints[0].x, y: variableMean },
+        { x: dataPoints[dataPoints.length - 1].x, y: variableMean },
+      ]
+    : [];
 
   return (
-    <VictoryChart theme={VictoryTheme.material}>
-      <VictoryLine data={dataPoints} />
+    <VictoryChart theme={graphTheme}>
+      <VictoryLegend
+        data={[{ name: label }].concat(
+          variableMean ? [{ name: "Population Mean" }] : []
+        )}
+        colorScale={[varColor, meanColor]}
+        x={200}
+        y={40}
+      />
+      <VictoryLine
+        data={dataPoints}
+        style={{
+          data: { stroke: varColor, strokeWidth: 2 },
+        }}
+      />
+      <VictoryLine
+        data={meanLine}
+        style={{
+          data: { stroke: meanColor, strokeWidth: 2, strokeDasharray: 5 },
+        }}
+      />
       <VictoryAxis
         scale="time"
         label={label}
