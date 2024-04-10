b6a60ab9ecacf75fcd254ada012bf91e1de685ae
LoganMartinez <lmartin9@trinity.edu>
2024-04-10 11:11:15 -0500

    added contact

--- AgingHealthApp/components/navigation/AppHeader.tsx
+++ AgingHealthApp/components/navigation/AppHeader.tsx
@@ -3,6 +3,7 @@ import { useAuth } from "../authentication/AuthProvider";
 import { StyleSheet } from "react-native";
 import { useState } from "react";
 import { useNav } from "./NavigationProvider";
+import ContactModal from "../help/ContactModal";
 
 type Props = {
   title: string;
@@ -13,6 +14,7 @@ const AppHeader = ({ title, onBack = null }: Props) => {
   const auth = useAuth();
   const nav = useNav();
 
+  const [contactOpen, setContactOpen] = useState(false);
   const [helpMenuVisible, setHelpMenuVisible] = useState(false);
 
   const logout = () => {
@@ -21,31 +23,38 @@ const AppHeader = ({ title, onBack = null }: Props) => {
   };
 
   return (
-    <Appbar.Header>
-      {onBack && <Appbar.BackAction onPress={onBack} />}
-      <Appbar.Content title={title} />
-      <Menu
-        visible={helpMenuVisible}
-        onDismiss={() => setHelpMenuVisible(false)}
-        anchor={
-          <IconButton
-            icon="help-circle-outline"
-            onPress={() => setHelpMenuVisible(true)}
+    <>
+      <ContactModal open={contactOpen} setOpen={setContactOpen} />
+      <Appbar.Header>
+        {onBack && <Appbar.BackAction onPress={onBack} />}
+        <Appbar.Content title={title} />
+        <Menu
+          visible={helpMenuVisible}
+          onDismiss={() => setHelpMenuVisible(false)}
+          anchor={
+            <IconButton
+              icon="help-circle-outline"
+              onPress={() => setHelpMenuVisible(true)}
+            />
+          }
+          anchorPosition="bottom"
+        >
+          <Menu.Item
+            title="App Walkthrough"
+            leadingIcon="help"
+            onPress={nav.startOnboarding}
           />
-        }
-        anchorPosition="bottom"
-      >
-        <Menu.Item
-          title="App Walkthrough"
-          leadingIcon="help"
-          onPress={nav.startOnboarding}
-        />
-        <Menu.Item
-          title="Contact Support"
-          leadingIcon="chat-question-outline"
-        />
-      </Menu>
-    </Appbar.Header>
+          <Menu.Item
+            title="Contact Support"
+            leadingIcon="chat-question-outline"
+            onPress={() => {
+              setHelpMenuVisible(false);
+              setContactOpen(true);
+            }}
+          />
+        </Menu>
+      </Appbar.Header>
+    </>
   );
 };
 
