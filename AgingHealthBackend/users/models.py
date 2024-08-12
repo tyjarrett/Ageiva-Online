f69a3771d4d81a069c77d783752fff77c9b3d07e
unknown <tjarrett@trinity.edu>
2024-08-12 09:30:54 -0500

    migrations

--- AgingHealthBackend/users/models.py
+++ AgingHealthBackend/users/models.py
@@ -32,6 +32,11 @@ class User(AbstractUser):
     USERNAME_FIELD="email"
     REQUIRED_FIELDS=[]
 
+class PasswordReset(models.Model):
+    email = models.EmailField()
+    token = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
     
 
     
